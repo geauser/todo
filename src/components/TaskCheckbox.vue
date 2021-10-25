f01edb0ea1ae3f2530d4f111c100172a54f6fbf5
geauser <geauser@protonmail.com>
2021-10-25 12:16:53 +0200

    feat: add <task-checkbox> component along with pretty-checkbox dependency

---
+++ src/components/TaskCheckbox.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <div
+    class="pretty p-default p-round"
+    style="margin-right: 0">
+
+    <input
+      type="checkbox"
+      :checked="checked"
+      @click.prevent="$emit('click')" />
+
+    <div class="state">
+      <label />
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+
+  props: {
+    checked: { type: Boolean, required: true },
+  },
+
+  emits: ['click'],
+
+};
+</script>
