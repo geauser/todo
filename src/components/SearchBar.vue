75c123c7476bc336e2163f162867b61480c32258
geauser <geauser@protonmail.com>
2021-10-25 13:01:15 +0200

    feat: add dummy search bar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <input
+    class="px-2 py-0.5 font-sans text-sm text-gray-900 placeholder-gray-400 bg-white border border-gray-200 rounded"
+    type="text"
+    placeholder="Search">
+
+</template>
+
+<script>
+export default {
+
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+
+};
+</script>
+
+<style>
+
+</style>
